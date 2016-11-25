bcbf620564241fd6299dbc6d3a0534d6d9b0b564
Travis Jeffery <tj@travisjeffery.com>
2016-11-24 19:54:41 -0500

    Initial commit

---
+++ simplelog.go
@@ -0,0 +1,55 @@
+package simplelog
+
+import (
+	"fmt"
+	"io"
+	"sync"
+	"time"
+)
+
+type Level int
+
+const (
+	DEBUG Level = iota
+	INFO
+)
+
+var levels = map[Level]string{
+	DEBUG: "DEBUG",
+	INFO:  "INFO",
+}
+
+type Logger struct {
+	Writer io.Writer
+	Level  Level
+	Prefix string
+	sync.Mutex
+}
+
+func New(writer io.Writer, level Level, prefix string) *Logger {
+	return &Logger{
+		Writer: writer,
+		Level:  level,
+		Prefix: prefix,
+	}
+}
+
+func (l *Logger) log(level Level, msg string, args ...interface{}) error {
+	l.Lock()
+	defer l.Unlock()
+	if level > l.Level {
+		return nil
+	}
+	ts := time.Now().Format("2006-01-02 15:04:05")
+	f := fmt.Sprintf("%s [%s] %s: %s\n", ts, levels[level], l.Prefix, msg)
+	_, err := fmt.Fprintf(l.Writer, f, args...)
+	return err
+}
+
+func (l *Logger) Info(msg string, args ...interface{}) error {
+	return l.log(INFO, msg, args...)
+}
+
+func (l *Logger) Debug(msg string, args ...interface{}) error {
+	return l.log(DEBUG, msg, args...)
+}
